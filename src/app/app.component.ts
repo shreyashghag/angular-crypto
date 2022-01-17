29a0bdf74a36ed62423c5d539b63b2f0d1337dcd
shreyashghag <shreyashghag346@gmail.com>
2022-01-17 16:51:33 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import * as CryptoJS from 'crypto-js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-crypto';
+
+
+// Declare this key and iv values in declaration
+private key = CryptoJS.enc.Utf8.parse('4512631236589784');
+private iv = CryptoJS.enc.Utf8.parse('4512631236589784');
+
+// Methods for the encrypt and decrypt Using AES
+encryptUsingAES256() {
+    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(JSON.stringify("Your Json Object data or string")), this.key, {
+        keySize: 128 / 8,
+        iv: this.iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    console.log('Encrypted :' + encrypted);
+    this.decryptUsingAES256(encrypted.toString());
+    return encrypted;
+}
+
+decryptUsingAES256(decString: string) {
+    var decrypted = CryptoJS.AES.decrypt(decString, this.key, {
+        keySize: 128 / 8,
+        iv: this.iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    console.log('Decrypted : ' + decrypted);
+    console.log('utf8 = ' + decrypted.toString(CryptoJS.enc.Utf8));
+
+}
+
+ngOnInit() {
+  this.encryptUsingAES256();
+}
+
+
+
 }
